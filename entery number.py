6a6f80bb3d2cd4ea1e38e1a116318ef389ad9ae1
hamid <hemloblack@gmail.com>
2025-12-02 20:51:02 +0330

    Add files via upload

---
+++ entery number.py
@@ -0,0 +1,12 @@
+def sum_number(*arge):
+    listnum=[]
+    for i in arge:
+       
+        if i%2==0:
+            listnum.append(i)
+    return listnum
+b=sum_number(3
+             
+             
+             )
+print(b)        
