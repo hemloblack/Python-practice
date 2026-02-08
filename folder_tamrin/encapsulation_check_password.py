ae391114aae452acbc9f0433a072c6b6382aff03
hamid <hemloblack@gmail.com>
2026-02-08 17:45:06 +0330

    Create encapsulation_check_password.py

---
+++ folder_tamrin/encapsulation_check_password.py
@@ -0,0 +1,22 @@
+class User:
+    def __init__(self, username, password):
+        self.__username = username
+        self.__password = password
+
+    def login(self):
+        if self.__password == 123456:
+            print(f"login successful!! | Welcome {self.__username} :)")
+        else:
+            print(f"access denied!! | incorrect password {self.__username} :(")
+            
+            
+            
+username = input("enter username: ")
+
+try:
+    password = int(input("enter password: "))
+except ValueError:
+    print("Error: password must be numeric (integer)")
+else:
+    user = User(username, password)
+    user.login()
