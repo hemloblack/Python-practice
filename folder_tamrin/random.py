e8bc1d55604d4020e1809e235d66aa07a2bceefa
hamid <hemloblack@gmail.com>
2026-02-14 21:21:17 +0330

    Update random.py

--- folder_tamrin/random.py
+++ folder_tamrin/random.py
@@ -5,7 +5,7 @@ print(random.randrange(0,10,2))# عدد رندوم بین این 0تا 10  با 
 colors = ["red", "blue", "green"]
 print(random.choices(colors,k=5))#انتخاب از بین ایتم ها مجاز به  تکرار ایتم
 print(random.sample(colors,k=3))#انتخاب از بین ایتم ها بدون تکرار ایتم
-cards = [1, 2, 3, 4]
+cards = [1, 2, 3, 4,5]
 random.shuffle(cards)# برای بهم زدن ترتیب لیست(بُر زدن لیست)
 print(cards)
 print(random.uniform(1.3,8.5))#انتخاب عدد رندومه اعشاری
