de13b05a0ea39d72fc0e556f5dcddd558d6937bb
hamid <hemloblack@gmail.com>
2026-02-19 23:53:08 +0330

    Update random.py

--- folder_tamrin/random.py
+++ folder_tamrin/random.py
@@ -2,7 +2,7 @@ import random
 print(random.random())
 print(random.randint(1,2))
 print(random.randrange(0,10,2))# عدد رندوم بین این 0تا 10  با گام های 2 تایی (یعنی دوتا دوتا میره تا 10 و یکی رو انتخاب میکنه)
-colors = ["red", "blue", "green"]
+colors = ["red", "blue", " green"]
 print(random.choices(colors,k=5))#انتخاب از بین ایتم ها مجاز به  تکرار ایتم
 print(random.sample(colors,k=3))#انتخاب از بین ایتم ها بدون تکرار ایتم
 cards = [1, 2, 3, 4,5]
