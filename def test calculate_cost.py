63906a61d8fda8ff08a236380bbb3e620e7ade87
hamid <hemloblack@gmail.com>
2026-04-18 23:04:52 +0330

    Update def test calculate_cost.py

--- def test calculate_cost.py
+++ def test calculate_cost.py
@@ -2,4 +2,4 @@ def calculate_cost(gigabytes,cost_per_gb):
     sum=(gigabytes*cost_per_gb)
     return sum
 
-print(calculate_cost(int(input("Enter GB used:?")),(float(input("Enter cost per GB")))))
+print(calculate_cost(int(input("Enter GB used:?")),(float(input("Enter cost pe GB")))))
